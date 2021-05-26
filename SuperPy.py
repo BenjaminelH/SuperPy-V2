c976402c1559be373b6ecdd03f421d59032cfdaa
BenjaminelH <80705371+BenjaminelH@users.noreply.github.com>
2021-05-26 19:28:03 +0200

    Update SuperPy.py

--- SuperPy.py
+++ SuperPy.py
@@ -2,9 +2,12 @@ import sys
 import csv
 from texttable import Texttable
 from datetime import timedelta, datetime
-
 #Function to get inventory items
 def get_inventory():
+   '''
+      Reads the bought.csv file row by row and return
+      the grand list of all the rows
+   '''
    with open('bought.csv', mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     all_rows = []
@@ -14,6 +17,10 @@ def get_inventory():
 
 
 def validate_date(date_string):
+   '''
+      This function accepts the input string (date) and check whether the date is valid or not
+      usually it is used in the command like buy product or sell product
+   '''
    try:
       datetime.strptime(date_string, '%Y-%m-%d')
       return True
@@ -23,11 +30,20 @@ def validate_date(date_string):
 
 
 def get_date():
+   '''
+      Function that reads the text file where the date is stored.
+      The date is stored in txt file becuase we have to advance the time
+      some times.
+   '''
    file_contents = open("current_date.txt", "r").read()
    return file_contents
 
 
 def buy_product(id, product_name, price, expiry, buy_date):
+   '''
+      Takes product name, price, expiry and buy date as arguments
+      and open bought.csv file. Then it writes the data to the csv file
+   '''
    with open('bought.csv', mode='a', newline='') as file:
        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
        writer.writerow([id, product_name, buy_date, price, expiry])
@@ -35,6 +51,12 @@ def buy_product(id, product_name, price, expiry, buy_date):
 
 
 def sell_product(id, product_name, product_price, inventory, current_date):
+   '''
+      Initializes the product to None, that;s because if the product
+      is not found it will display an error.
+      If the product is found it will open the sold.csv file and write the information
+      to that file.
+   '''
    product = None
    for item in inventory:
       if item[1] == product_name:
@@ -50,6 +72,12 @@ def sell_product(id, product_name, product_price, inventory, current_date):
 
 
 def report_inventory(current_date, inventory, day):
+   '''
+      This function prints the inventory on the basis of the day
+      selected. So if the value of day is yesterday, it decreases the day and
+      displays the data accordingly. Otherwise it displays the data
+      on the basis of current date read from current_date.txt file
+   '''
    inventory_to_display = []
    inventory_to_display.append(inventory[0])
    if day == "--yesterday":
@@ -65,6 +93,10 @@ def report_inventory(current_date, inventory, day):
 
 
 def advance_time(days_to_increase):
+   '''
+      Reads the current_date.txt file and increases
+      the date on the basis of days entered by the user in the command
+   '''
    current_date = get_date()
    dt = datetime.strptime(current_date, '%Y-%m-%d')
    yesterday = dt + timedelta(days = days_to_increase)
@@ -76,6 +108,11 @@ def advance_time(days_to_increase):
 
 
 def report_revenue(report_date, current_date):
+   '''
+      This function reads the csv file and check if the current date is
+      equal to the date requested. If the date matches it calculated the revenue
+      and prints the information.
+   '''
    revenue = 0
    if report_date[0] == "--yesterday":
       current_date = get_date()
@@ -130,3 +167,4 @@ def main():
       advance_time(int(args[2]))
 if __name__ == "__main__":
    main()
+
